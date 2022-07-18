c7492983da3abe6f1a262268a5eded126e80b91b
Elias Lundheim <elias.lundheim@gmail.com>
2022-07-18 11:35:28 +0200

    Initial firebase install

--- craver_flutter_project/lib/main.dart
+++ craver_flutter_project/lib/main.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
 import 'bottom_nav.dart';
 import 'pages/alarms.dart';
-
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 //Base:
 //https://blog.logrocket.com/how-to-build-a-bottom-navigation-bar-in-flutter/
 
 void main() async {
   initi_notifications();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
--- craver_flutter_project/macos/Flutter/GeneratedPluginRegistrant.swift
+++ craver_flutter_project/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,11 +5,13 @@
 import FlutterMacOS
 import Foundation
 
+import firebase_core
 import flutter_local_notifications
 import flutter_native_timezone
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
   FlutterNativeTimezonePlugin.register(with: registry.registrar(forPlugin: "FlutterNativeTimezonePlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
