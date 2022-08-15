3a4246997b6c1dbcffd0dd2951e8240155b1c045
Elias Lundheim <elias.lundheim@cern.ch>
2022-08-15 17:49:22 +0200

    update allowed stats and home page and waitress

--- docker_server/database_scripts/requestHandler.py
+++ docker_server/database_scripts/requestHandler.py
@@ -5,7 +5,7 @@ from config import client_user
 from lblogbookInterface import LbLogbook
 from prometheusInterface import Prometheus
 from controlPanelInterface import ControlPanel
-from flask import Flask, request
+from flask import Flask, request, redirect
 import json
 import datetime
 
@@ -15,6 +15,10 @@ app = Flask(__name__)
 P = Prometheus()
 L = LbLogbook()
 C = ControlPanel()
+
+@app.route("/")
+def home_page():
+    return redirect("https://home.web.cern.ch/")
 
 # Example: /prometheus_query?command=up&instance=aseb03.lbdaq.cern.ch&time=4
 # Example: /prometheus_query?command=up
@@ -50,7 +54,8 @@ def get_control_panel_data():
     return C.get(state)
 
 
+
 if __name__ == '__main__':
-    #Remember to disable debug sooner or later
-    app.run(host="0.0.0.0", debug=False
-    , port=8080)
+    from waitress import serve
+    #app.run(host="0.0.0.0", port=8080)
+    serve(app, host="0.0.0.0", port=8080)
