48da92e2da6bcdec0dc06e16c7b96f770bd777b5
Elias Lundheim <elias.lundheim@cern.ch>
2022-08-16 11:54:36 +0200

    more threads and stuff

--- docker_server/database_scripts/requestHandler.py
+++ docker_server/database_scripts/requestHandler.py
@@ -64,4 +64,4 @@ def get_server_version():
 if __name__ == '__main__':
     from waitress import serve
     #app.run(host="0.0.0.0", port=8080)
-    serve(app, host="0.0.0.0", port=8080)
+    serve(app, host="0.0.0.0", port=8080, threads=8, connection_limit=1000, backlog=4000)
